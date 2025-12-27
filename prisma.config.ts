d1dcf0838f4a109e0d00fceb4a525cf08b97c514
Нагима Кенеуова <71884074+Nagimulia@users.noreply.github.com>
2025-12-27 17:20:34 +0500

    h

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,6 +3,7 @@
 // npm install --save-dev prisma dotenv
 
 import { defineConfig, env } from "prisma/config";
+import 'dotenv/config';
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,6 @@
 // create a prisma instance and cache in development
 import { PrismaClient } from "@prisma/client";
 
-
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient();
